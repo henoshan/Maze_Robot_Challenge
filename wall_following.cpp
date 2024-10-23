e7597358661b5fed55fd11c8db2924e2d702b970
Tharmilan Thanabalasingam <129476295+TTharmilan@users.noreply.github.com>
2024-10-23 21:23:58 +0530

    Update wall_following.cpp

--- wall_following.cpp
+++ wall_following.cpp
@@ -22,28 +22,28 @@ int forwardLeftSpeed = 80; // for moving forward--> adjusted
 int forwardRightSpeed = 86;
 int turnLeftSpeed = 80;  // for turning right,Left and around --> constant
 int turnRightSpeed = 86;
-int offset = 25;
-int offsetMin = 15;
-// const int moveForwardDelay = 10;
+int offset = 12;
+int offsetMin = 8;
+int offsetTurn = 18; //18
 int L, R, F, T = 0;
-// char path[100];  // Array to store moves
-// int pathIndex = 0;  // Current index of the path array
+int path[200];  // Array to store moves
+int pathIndex = 0;  // Current index of the path array
 
 // Function Prototypes
 long readUltrasonic(int trigPin, int echoPin);
 void moveForward();
-void startRobot();
-void stopRobot();
+void startRobot(int Sl,int Sr);
+void stopRobot(int d=1);
 void turnLeft();
 void turnRight();
 void turnAround();
 void adjustLeft(int &lSpeed, int &rSpeed);
 void adjustRight(int &lSpeed, int &rSpeed);
-void moveBackward();
+void moveBackward(int d);
 // void followPath();
 // void backtrack();
-// void addToPath(char movement);
-// long readUltrasonicFiltered(int trigPin, int echoPin); 
+void addToPath(char movement);
+long readUltrasonicFiltered(int trigPin, int echoPin); 
 
 void setup() {
   // Initialize sensor pins
@@ -63,18 +63,17 @@ void setup() {
   pinMode(rightBackward, OUTPUT);
   pinMode(enableRight, OUTPUT);
 
-  Serial.begin(9600); // for testing ultrasonic
-  stopRobot();
+  // Serial.begin(9600); // for testing ultrasonic
+  stopRobot(0);
 }
 
 void loop() {
+  delay(50);
   // Read distances from the ultrasonic sensors and store them in variables
   long frontDist = readUltrasonic(trigFront, echoFront);
   long leftDist = readUltrasonic(trigLeft, echoLeft);
   long rightDist = readUltrasonic(trigRight, echoRight);
   // int irValue = analogRead(irPin); // Read IR sensor value
-  Serial.print(" iam loop ");
-
   // // Check for the endpoint using the IR sensor
   // if (irValue < 200) { // Adjust threshold based on your IR sensor
   //   stopRobot();
@@ -82,84 +81,95 @@ void loop() {
   //   delay(5000); // Stop at the end point
   // }
 
-  if ((frontDist < offsetMin || frontDist > 250) && leftDist < offsetMin && rightDist < offsetMin) {
-    stopRobot();
-    turnAround();
-  } else if (leftDist > offset || rightDist > offset) {
-    // Obstacle ahead: choose left or right
-    if ((leftDist > offset && rightDist > offset) || (leftDist < offset && rightDist > offset)) {
-      stopRobot();
+  if(leftDist > offsetTurn || rightDist > offsetTurn){
+    if((rightDist > offsetTurn) && (leftDist > offsetTurn)){
+      if(readUltrasonic(trigFront, echoFront) < offsetMin ){
+          moveBackward(150);
+      }
+      if(R%4==0){
+        turnLeft();
+      }else{
+        turnRight();
+      }
+    }else if(rightDist > offsetTurn && leftDist < offsetTurn){
+      if(readUltrasonic(trigFront, echoFront) < offsetMin ){
+        moveBackward(150);
+      }
       turnRight();
-    } else if (leftDist > offset && rightDist < offset) {
-      stopRobot();
-      turnLeft();
+    }else{
+      if(readUltrasonic(trigFront, echoFront) < offsetMin ){
+        moveBackward(150);
+      }
+      turnLeft();   
     }
-  } else if ((frontDist > offset) && (leftDist < offsetMin) && (rightDist < offsetMin)) {
-    moveForward();// Move forward
-  } else if (frontDist < 5 || frontDist > 250) {
-    moveBackward();
+  }else{
+    frontDist = readUltrasonic(trigFront, echoFront);
+    if(frontDist < offset || frontDist > 300){   //&&(rightDist < offset)&&(leftDist < offset)
+      if(readUltrasonic(trigFront, echoFront) < offsetMin ){
+        moveBackward(150);
+      }
+      turnAround();
+    }else moveForward();
+  }
+  // move backward if too clode to wall
+  frontDist = readUltrasonic(trigFront, echoFront);
+  if(frontDist < offsetMin || frontDist > 300){
+    moveBackward(100);
   }
 }
 
 // Functions //
-
 // Ultrasonic sensor function
 long readUltrasonic(int trigPin, int echoPin) {
   digitalWrite(trigPin, LOW);
-  delayMicroseconds(4); // check and increase for better iterative reading
+  delayMicroseconds(10); // check and increase for better iterative reading
   digitalWrite(trigPin, HIGH);
-  delayMicroseconds(20);
+  delayMicroseconds(40);
   digitalWrite(trigPin, LOW);
-  long duration = pulseIn(echoPin, HIGH,15000); //max range 255cm
+  long duration = pulseIn(echoPin, HIGH,20000); //limit max range
+  if(duration == 0){
+    duration=20000;
+  }
+  delay(10);
   return duration * 0.034 / 2; // Distance in cm ( increased precision may require more time--> more delay in getting the readings)
 }
-// long readUltrasonicFiltered(int trigPin, int echoPin) {  // filtered for echo
-//   long totalDistance = 0;
-//   int numReadings = 5; // Take 5 readings and average them
 
-//   for (int i = 0; i < numReadings; i++) {
-//     totalDistance += readUltrasonic(trigPin, echoPin);
-//     delay(10); // Delay between each reading to avoid echo overlap
-//   }
-  
-//   return totalDistance / numReadings; // Return the average distance
-// }
-
-void startRobot() {
-  analogWrite(enableLeft, forwardLeftSpeed);
-  analogWrite(enableRight, forwardRightSpeed);
+void startRobot(int Sl,int Sr) {
+  analogWrite(enableLeft, forwardLeftSpeed + Sl);
+  analogWrite(enableRight, forwardRightSpeed + Sr);
   digitalWrite(leftForward, HIGH);
   digitalWrite(leftBackward, LOW);
   digitalWrite(rightForward, HIGH);
   digitalWrite(rightBackward, LOW);
 }
 
-void moveBackward() {
+void moveBackward(int d) {
   analogWrite(enableLeft, forwardLeftSpeed);
   analogWrite(enableRight, forwardRightSpeed);
   digitalWrite(leftForward, LOW);
   digitalWrite(leftBackward, HIGH);
   digitalWrite(rightForward, LOW);
   digitalWrite(rightBackward, HIGH);
-  delay(200);
+  delay(d);
   stopRobot();
 }
 
-void stopRobot() {
+void stopRobot(int d) {
+  delay(d);
   analogWrite(enableLeft, 0);
   analogWrite(enableRight, 0);
   digitalWrite(leftForward, LOW);
   digitalWrite(leftBackward, LOW);
   digitalWrite(rightForward, LOW);
   digitalWrite(rightBackward, LOW);
-  delay(100); // check and adjust
+  delay(300); // check and adjust
 }
 
 void moveForward() {
   int lSpeed = forwardLeftSpeed;
   int rSpeed = forwardRightSpeed;
-  lSpeed = constrain(lSpeed, 0, 255);
-  rSpeed = constrain(rSpeed, 0, 255);
+  lSpeed = constrain(lSpeed, 70, 100);
+  rSpeed = constrain(rSpeed, 70, 100);
 
   analogWrite(enableLeft, lSpeed);
   analogWrite(enableRight, rSpeed);
@@ -168,115 +178,113 @@ void moveForward() {
   digitalWrite(rightForward, HIGH);
   digitalWrite(rightBackward, LOW);
 
-  Serial.println("Moving forward");
   long frontDist = readUltrasonic(trigFront, echoFront);
   long leftDist = readUltrasonic(trigLeft, echoLeft);
   long rightDist = readUltrasonic(trigRight, echoRight);
-
-  while (!(frontDist < offset || frontDist > 300 || leftDist > offset || rightDist > offset)) {
-    if (leftDist > rightDist) {
-      adjustRight(lSpeed, rSpeed); // Too close to the left wall: adjust to the right
-    } else {
-      adjustLeft(lSpeed, rSpeed);  // Too close to the right wall: adjust to the left
+  int initialDiff = abs(leftDist-rightDist);
+  while (!((frontDist < offset || frontDist > 300) || leftDist > offsetTurn || rightDist > offsetTurn)) {
+    if(initialDiff != abs(leftDist-rightDist)){
+      if (leftDist > rightDist) {
+        adjustRight(lSpeed, rSpeed); // Too close to the left wall: adjust to the right
+      } else {
+        adjustLeft(lSpeed, rSpeed);  // Too close to the right wall: adjust to the left
+      }
     }
-    delay(100);
-
+    delay(10);
     // Update sensor readings inside the loop
     frontDist = readUltrasonic(trigFront, echoFront);
     leftDist = readUltrasonic(trigLeft, echoLeft);
     rightDist = readUltrasonic(trigRight, echoRight);
   }
-
-  stopRobot();
-  // addToPath('F');
+  stopRobot(10);
+  startRobot(0,0);
+  long startf =millis();
+  while((readUltrasonic(trigFront, echoFront) > offsetMin-3) && ((millis()-startf) < 300)){ // move forward a bit more // check front >300
+    delay(10);
+  }
+  startf=0;
+  stopRobot(80);
+  addToPath(1); //front =1
   F++;
 }
 
 void turnLeft() {
-  if(L>=1){  // dont move forward if there is a opening in the start
-  //  if(!(path[pathIndex]=='R' || path[pathIndex]=='L')){
-  //   startRobot();
-  //   delay(300);
-  //   stopRobot();
-  //  }
-    startRobot();
-    delay(300);
-    stopRobot();
-  }
+  // if(readUltrasonic(trigFront, echoFront) < offset ){
+  //   moveBackward(150);
+  // }
   digitalWrite(leftForward, LOW);
-  digitalWrite(leftBackward, HIGH);
+  digitalWrite(leftBackward, LOW);
   digitalWrite(rightForward, HIGH);
   digitalWrite(rightBackward, LOW);
-
-  analogWrite(enableLeft, turnLeftSpeed);
+  // analogWrite(enableLeft, turnLeftSpeed);
   analogWrite(enableRight, turnRightSpeed);
-
-  long leftDist = readUltrasonic(trigLeft, echoLeft);
-
-  while (!(leftDist < offset)) {
-    leftDist = readUltrasonic(trigLeft, echoLeft);
+  long startl =millis();
+  while ((readUltrasonic(trigLeft, echoLeft) > offsetMin) && ((millis()-startl) < 530)) {
+    delay(20);
   }
-  delay(50); // turn to 90 degrees
   stopRobot();
-  // addToPath('L');
+  addToPath(2); // left =2
   L++;
-  startRobot();
-  delay(250);
-  stopRobot();
+
+  startRobot(0,4);
+  startl =millis();
+  while((readUltrasonic(trigFront, echoFront) > offset) && ((millis()-startl) < 260)){ // move forward a bit more
+    delay(10);
+  }
+  startl=0;
+  stopRobot(40);
 }
 
 void turnRight() {
-  if(R>=1){  // dont move forward if there is a opening in the start
-  //  if(!(path[pathIndex]=='R' || path[pathIndex]=='L')){ // for continuous turns
-  //   startRobot();
-  //   delay(300);
-  //   stopRobot();
-  //  }
-    startRobot();
-    delay(300);
-    stopRobot();
-  }
+  // if(readUltrasonic(trigFront, echoFront) < offset ){
+  //   moveBackward(150);
+  // }
   digitalWrite(leftForward, HIGH);
   digitalWrite(leftBackward, LOW);
   digitalWrite(rightForward, LOW);
-  digitalWrite(rightBackward, HIGH);
-
+  digitalWrite(rightBackward, LOW);
   analogWrite(enableLeft, turnLeftSpeed);
-  analogWrite(enableRight, turnRightSpeed);
-
-  long rightDist = readUltrasonic(trigRight, echoRight);
-
-  while (!(rightDist < offset)) {
-    rightDist = readUltrasonic(trigRight, echoRight);
+  // analogWrite(enableRight, turnRightSpeed);
+  long startr =millis();
+  while ((readUltrasonic(trigRight, echoRight) > offsetMin) && ((millis()-startr) < 530)) {
+    delay(20);
   }
-  delay(50);
   stopRobot();
-  // addToPath('R');
+  addToPath(3); // right = 3
   R++;
-  startRobot();
-  delay(250);   // for contiuous turns it will fail
-  stopRobot();
+  startRobot(4,0);
+  startr =millis();
+  while((readUltrasonic(trigFront, echoFront) > offset) && ((millis()-startr) < 260)){ // move forward a bit more
+    delay(10);
+  }
+  startr=0;
+  stopRobot(40);
 }
 
 void turnAround() {
-  analogWrite(enableLeft, forwardLeftSpeed);
-  analogWrite(enableRight, forwardRightSpeed);
+  analogWrite(enableLeft, forwardLeftSpeed-10);
+  analogWrite(enableRight, forwardRightSpeed-9);
   digitalWrite(leftForward, HIGH);
   digitalWrite(leftBackward, LOW);
   digitalWrite(rightForward, LOW);
   digitalWrite(rightBackward, HIGH);
 
-  long frontDist = readUltrasonic(trigFront, echoFront);
-  long leftDist = readUltrasonic(trigLeft, echoLeft);
-  long rightDist = readUltrasonic(trigRight, echoRight);
-
-  while (!((frontDist > offset) && (leftDist < offsetMin) && (rightDist < offsetMin))) {
-    frontDist = readUltrasonic(trigFront, echoFront);
-    leftDist = readUltrasonic(trigLeft, echoLeft);
-    rightDist = readUltrasonic(trigRight, echoRight);
+  long fD = readUltrasonic(trigFront, echoFront);
+  long lD = readUltrasonic(trigLeft, echoLeft);
+  long rD = readUltrasonic(trigRight, echoRight);
+  int startT= millis();
+  while (!((fD > offset*2) && (lD < offset) && (rD < offset))) {
+    delay(10);
+    fD = readUltrasonic(trigFront, echoFront);
+    lD = readUltrasonic(trigLeft, echoLeft);
+    rD = readUltrasonic(trigRight, echoRight);
+    if ((millis()-startT) >=1000){
+      break;
+    }
   }
 
   stopRobot();
+  addToPath(4); // turnaround = 4 
   // backtrack(); // remove the last movement
   T++;
 }
@@ -297,11 +305,11 @@ void adjustRight(int &lSpeed, int &rSpeed) {
 
 
 // Function to add movement to the path
-// void addToPath(char movement) {
-//   if (pathIndex < 100) {
-//     path[pathIndex++] = movement;
-//   }
-// }
+void addToPath(int movement) {
+  if (pathIndex < 200) {
+    path[pathIndex++] = movement;
+  }
+}
 // void backtrack() {
 //   // Remove the last movement from the path
 //   if (pathIndex > 0) {
@@ -327,4 +335,3 @@ void adjustRight(int &lSpeed, int &rSpeed) {
 //     }
 //   }
 // }
-
